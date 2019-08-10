dd39842fabf89d6e9587d3a0aa4aa675451370f8
Lok Swen <chonglokswen97@hotmail.com>
2019-08-10 23:59:06 +0800

    i hope this wasnt overwritten

---
+++ Assets/PlayerJoin.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerJoin : MonoBehaviour
+{
+    public GameObject panel;
+    public TextMeshProUGUI textPressToJoin;
+    public GameObject model;
+    public string key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel = gameObject;
+        textPressToJoin = panel.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        model = panel.gameObject.GetComponentInChildren<GameObject>();
+        model.gameObject.SetActive(false);
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            if (textPressToJoin.IsActive())
+            {
+                textPressToJoin.gameObject.SetActive(false);
+                model.gameObject.SetActive(true);
+
+            }
+            else
+            {
+                textPressToJoin.gameObject.SetActive(true);
+                model.gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/PlayerJoin2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerJoin2 : MonoBehaviour
+{
+    public Image playerwheel;
+    public Color wheelcolour;
+    public string key;
+    private Color defaultcolour;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerwheel = gameObject.GetComponentInChildren<Image>();
+        //wheelcolour = new Color(255, 0, 0, 80/250);
+        defaultcolour = playerwheel.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            if (playerwheel.color == defaultcolour)
+            {
+                playerwheel.color = wheelcolour;
+            }
+            else
+            {
+                playerwheel.color = defaultcolour;
+            }
+        }
+    }
+}
