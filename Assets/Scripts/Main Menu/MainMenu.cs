afeb7a25fccf19074a334b56a3c4facb0ed007ba
leekwanmeng <kwanmeng_lee@mymail.sutd.edu.sg>
2019-08-11 22:08:22 +0800

    Changed circle, still fixing main menu UI

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Canvas cMenu;
+    public Canvas cLobby;
+    public Camera mCamera;
+
+
+    public void ToLobby()
+    {
+        cMenu.gameObject.SetActive(false);
+        cLobby.gameObject.SetActive(true);
+
+        mCamera.fieldOfView = 3f;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quitgame!!!");
+        Application.Quit();
+    }
+}
