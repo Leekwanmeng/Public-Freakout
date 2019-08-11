47db7f2b912f861db7fd3262aa188188affd2b6f
leekwanmeng <kwanmeng_lee@mymail.sutd.edu.sg>
2019-08-11 11:00:37 +0800

    Extinguisher animation

---
+++ Assets/Scripts/New Scripts/ExtinguisherFumes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtinguisherFumes : MonoBehaviour
+{
+    private PlayerState m_PlayerState;
+    private ParticleSystem m_ParticleSystem;
+    void Awake()
+    {
+        m_PlayerState = GetComponentInParent<PlayerState>();
+        m_ParticleSystem = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        Blow();
+    }
+
+    void Blow() {
+        if (m_PlayerState.m_HoldItemId == 2 && m_PlayerState.m_IsUsingStationaryItem) {
+            m_ParticleSystem.Play();
+        }
+        else {
+            m_ParticleSystem.Clear();
+            m_ParticleSystem.Stop();
+        }
+    }
+}
