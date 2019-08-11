786ae01f397ef47052508c41064e192e64615f0c
Lok Swen <chonglokswen97@hotmail.com>
2019-08-11 14:14:14 +0800

    integrated w main game

---
+++ Assets/UI/PlayerJoin.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// attached to each player in the lobby (not actual players), 
+// need to assign the player game object itself, the image of the playerwheel, 
+// the colour (alpha set to 150 for yellow, 120 for the rest), and 
+// also need to assign the key (assumed to be A for all controllers)
+
+public class PlayerJoin : MonoBehaviour
+{
+    public Image playerwheel;
+    public Color wheelcolour;
+    public string key;
+    public bool joined;
+    private Color defaultcolour;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerwheel = gameObject.GetComponentInChildren<Image>();
+        //wheelcolour = new Color(255, 0, 0, 80/250);
+        defaultcolour = playerwheel.color;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            if (playerwheel.color == defaultcolour)
+            {
+                playerwheel.color = wheelcolour;
+                joined = true;
+            }
+            else
+            {
+                playerwheel.color = defaultcolour;
+                joined = false;
+            }
+        }
+    }
+}
