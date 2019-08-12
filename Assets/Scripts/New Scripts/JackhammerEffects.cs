18aaf19ef0bec0ca90194ecbd69859e29d269b1e
leekwanmeng <kwanmeng_lee@mymail.sutd.edu.sg>
2019-08-12 15:31:12 +0800

    Jackhammer vfx

---
+++ Assets/Scripts/New Scripts/JackhammerEffects.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JackhammerEffects : MonoBehaviour
+{
+    private PlayerState m_PlayerState;
+    private ParticleSystem[] m_ParticleSystems;
+    void Awake()
+    {
+        m_PlayerState = GetComponentInParent<PlayerState>();
+        m_ParticleSystems = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        Dust();
+    }
+
+    void Dust() {
+        if (m_PlayerState.m_HoldItemId == 3 && m_PlayerState.m_IsUsingStationaryItem) {
+            
+            foreach (ParticleSystem ps in m_ParticleSystems) {
+                ps.Play();
+            }
+        }
+        else {
+            foreach (ParticleSystem ps in m_ParticleSystems) {
+                ps.Clear();
+                ps.Stop();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/New Scripts/NoRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoRotation : MonoBehaviour
+{
+    private Quaternion m_RelativeRotation; 
+
+    void Start()
+    {
+        m_RelativeRotation = transform.localRotation;
+    }
+
+    void Update()
+    {
+        transform.rotation = m_RelativeRotation;
+    }
+}
