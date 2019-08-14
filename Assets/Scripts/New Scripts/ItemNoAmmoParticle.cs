d9fa7fcf03fda328df3c8dffeadd972af5c3e40d
leekwanmeng <kwanmeng_lee@mymail.sutd.edu.sg>
2019-08-15 02:03:14 +0800

    No ammo particles on items on floor

---
+++ Assets/Scripts/New Scripts/ItemNoAmmoParticle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemNoAmmoParticle : MonoBehaviour
+{
+    private Item m_Item;
+    private ParticleSystem m_ParticleSystem;
+    private bool m_NoAmmo;
+
+    void Awake()
+    {
+        m_Item = GetComponentInParent<Item>();
+        m_ParticleSystem = GetComponent<ParticleSystem>();
+    }
+
+    void Start()
+    {
+        m_NoAmmo = false;
+    }
+
+    void Update()
+    {
+        if (!m_NoAmmo) {
+            CheckEmptyAmmoSmoke();
+        }
+    }
+
+    void CheckEmptyAmmoSmoke() {
+        if (m_Item.m_Ammo <= 0f) {
+            m_ParticleSystem.Play();
+            m_NoAmmo = true;
+        }
+    
+    }   
+}
